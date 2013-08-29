18c6e807c238d3c1fdb7e05597d0107c73fbf13d
vinitha <vs.vinitha@gmail.com>
2013-08-29 12:23:57 +0530

    commit 1
    commit 1

---
+++ assets/www/js/indexOrg.js
@@ -0,0 +1,93 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+
+    blog: function(){
+        function getBlogs() {
+            var dfd = $.Deferred();
+            $.ajax({
+                url: 'http://alexbachuk.com/api/get_recent_posts/',
+                type: 'GET',
+                dataType: 'json',
+                success: function(data){
+                    var source   = $("#blog-template").html();
+                    var template = Handlebars.compile(source);
+                    var blogData = template(data);
+                    $('#blog-data').html(blogData);
+                    $('#blog-data').trigger('create');
+                    dfd.resolve(data);
+
+                },
+                error: function(data){
+                    console.log(data);
+                }
+            });
+            return dfd.promise();
+        };
+
+        getBlogs().then(function(data){
+            $('#all-posts').on('click','li', function(e){                
+                localStorage.setItem('postData', JSON.stringify(data.posts[$(this).index()]));
+            });
+        });
+
+        
+    },
+    single: function() {
+        
+            var postDataStorage = localStorage.getItem('postData');
+            var source   = $("#single-template").html();
+            var template = Handlebars.compile(source);
+            var postData = template(JSON.parse(postDataStorage));    
+            $('#single-data').html(postData);
+
+    },
+
+    portfolio: function(){
+        $.ajax({
+            url: 'http://alexbachuk.com/?json=get_recent_posts&post_type=portfolio',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data){
+                var source   = $("#portfolio-template").html();
+                var template = Handlebars.compile(source);
+                var portfolioData = template(data);
+                $('#portfolio-data').html(portfolioData);
+                $('#portfolio-data').trigger('create');
+
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
+    }
+
+};
